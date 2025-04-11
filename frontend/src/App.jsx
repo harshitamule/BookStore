090cc7046af64c75626743c97ccab691a5dacc3a
harshita <harshitamule95@gmail.com>
2025-04-11 11:39:46 +0530

    created routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import createBook from './pages/createBook';
+import showbook from './pages/showBook';
+import editBook from './pages/editBook';
+import deleteBook from './pages/deleteBook';
 
 const App = () => {
   return (
     <Routes>
-      {/* <Route path='' element = {}/> */}
-      
+      <Route path='/' element = {Home}/>
+      <Route path='/book/create' element = {createBook}/>
+      <Route path='/book/details/:id' element = {showbook}/>
+      <Route path='/book/edit/:id' element = {editBook}/>
+      <Route path='/book/delete/:id' element = {deleteBook}/>
     </Routes>
   )
 }
