228b27fd037bf807caeed37dd13cc6587fcdd366
harshita <harshitamule95@gmail.com>
2025-04-12 15:29:53 +0530

    create book and changed ui to black colour

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
-import createBook from './pages/createBook';
+import CreateBook from './pages/CreateBook';
 import ShowBook from './pages/showBook';
-import editBook from './pages/editBook';
-import deleteBook from './pages/deleteBook';
+import editBook from './pages/EditBook';
+import deleteBook from './pages/DeleteBook';
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element = {<Home/>}/>
-      <Route path='/books/create' element = {<createBook/>}/>
+      <Route path='/books/create' element = {<CreateBook/>}/>
       <Route path='/books/details/:id' element = {<ShowBook/>}/>
       <Route path='/books/edit/:id' element = {editBook}/>
       <Route path='/books/delete/:id' element = {<deleteBook/>}/>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,15 +35,15 @@ const Home = () => {
         <table className='w-full border-separate border-spacing-2'>
           <thead>
             <tr>
-              <th className='border border-slate-600 rounded-md'>No</th>
-              <th className='border border-slate-600 rounded-md'>Title</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>
+              <th className='border border-slate-300 rounded-md'>No</th>
+              <th className='border border-slate-300 rounded-md'>Title</th>
+              <th className='border border-slate-300 rounded-md max-md:hidden'>
                 Author
               </th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>
+              <th className='border border-slate-300 rounded-md max-md:hidden'>
                 Publish Year
               </th>
-              <th className='border border-slate-600 rounded-md'>Operations</th>
+              <th className='border border-slate-300 rounded-md'>Operations</th>
 
             </tr>
 
@@ -51,29 +51,29 @@ const Home = () => {
           <tbody>
             {books.map((books, index) =>(
               <tr key={books._id}>
-                <td className='border border-slate-700 rounded-md text-center'>
+                <td className='border border-slate-500 rounded-md text-center'>
                   {index+1}
                 </td>
-                <td className='border border-slate-700 rounded-md text-center'>
+                <td className='border border-slate-500 rounded-md text-center'>
                   {books.title}
                 </td>
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                <td className='border border-slate-500 rounded-md text-center max-md:hidden'>
                   {books.author}
                 </td>
-                <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+                <td className='border border-slate-500 rounded-md text-center max-md:hidden'>
                   {books.publishYear}
                 </td>
 
-                <td className='border border-slate-700 rounded-md text-center'>
+                <td className='border border-slate-500 rounded-md text-center'>
                   <div className='flex justify-center gap-x-4'>
                     <Link to={`/books/details/${books._id}`}>
-                      <BsInfoCircle className='text-2xl text-green-800'/>
+                      <BsInfoCircle className='text-2xl text-green-400'/>
                     </Link>
                     <Link to={`/books/edit/${books._id}`}>
-                      <AiOutlineEdit className='text-2xl text-yellow-600'/>
+                      <AiOutlineEdit className='text-2xl text-yellow-300'/>
                     </Link>
                     <Link to={`/books/delete/${books._id}`}>
-                      <MdOutlineDelete className='text-2xl text-red-600'/>
+                      <MdOutlineDelete className='text-2xl text-red-500'/>
                     </Link>
                   </div>
                 </td>
